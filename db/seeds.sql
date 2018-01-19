1ed675423185c98152ee9a61c07adb42d087daed
Terry Crouch <tcrouch@operationsports.com>
2018-01-19 13:24:32 -0600

    Base Schema
    Contains no user table or anything other than dummy questions and specialty assignments/weights

---
+++ db/seeds.sql
@@ -0,0 +1,38 @@
+USE baggage_db;
+
+INSERT INTO questions (question,spec1,spec1_wgt,spec2,spec2_wgt,spec3,spec3_wgt) VALUES
+	("Question #1 Goes here!",1,90,2,75,3,70),
+	("Question #2 Goes here!",2,85,3,80,4,65),
+	("Question #3 Goes here!",3,85,4,80,5,75),
+	("Question #4 Goes here!",4,95,5,90,1,85),
+	("Question #5 Goes here!",5,85,1,70,2,65),
+	("Question #6 Goes here!",1,90,2,80,3,70),
+	("Question #7 Goes here!",2,85,3,80,4,60),
+	("Question #8 Goes here!",3,95,4,75,5,55),
+	("Question #9 Goes here!",4,75,5,65,1,50),
+	("Question #10 Goes here!",5,70,1,65,2,45),
+	("Question #11 Goes here!",1,90,2,75,3,60),
+	("Question #12 Goes here!",2,75,3,70,4,65),
+	("Question #13 Goes here!",3,80,4,70,5,60),
+	("Question #14 Goes here!",4,55,5,45,1,35),
+	("Question #15 Goes here!",5,80,1,60,2,40),
+	("Question #16 Goes here!",1,95,2,80,3,65),
+	("Question #17 Goes here!",2,100,3,75,4,50),
+	("Question #18 Goes here!",3,90,4,85,5,75),
+	("Question #19 Goes here!",4,95,5,90,1,70),
+	("Question #20 Goes here!",5,70,1,65,2,50),
+	("Question #21 Goes here!",1,85,2,75,3,65),
+	("Question #22 Goes here!",2,90,3,80,4,60),
+	("Question #23 Goes here!",3,100,4,90,5,40),
+	("Question #24 Goes here!",4,75,5,70,1,65),
+	("Question #25 Goes here!",5,95,1,65,2,40)
+;
+
+
+INSERT INTO specialties (spec_name) VALUES
+	("Mental Health Specialty #1"),
+	("Mental Health Specialty #2"),
+	("Mental Health Specialty #3"),
+	("Mental Health Specialty #4"),
+	("Mental Health Specialty #5")
+;
