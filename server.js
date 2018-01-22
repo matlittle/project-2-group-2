386ce210d6d08f70222dde30c35fc6f105dfd0b4
matlittle <matlittle727@gmail.com>
2018-01-21 22:19:22 -0600

    Added basic dependencies, and implemented them in server.js file

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
+
+/* Dependencies */
 const express = require('express');
 const app = express();
 
 const PORT = 8081;
 
+// Add body parser as middleware to parse json in request bodies
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add OAuth server.
+var oauthServer = require('express-oauth-server');
+app.oauth = oauthServer({
+  model: require('./models/login-orm');
+});
 
 
+/* Examples for routes that will require authentication, vs public routes */
+// Get secret.
+app.get('/secret', app.oauth.authenticate(), function(req, res) {
+  // Will require a valid access_token.
+  res.send('Secret area');
+});
 
+app.get('/public', function(req, res) {
+  // Does not require an access_token.
+  res.send('Public area');
+});
+
+/* End examples */
+
+
+// Start listening for requests.
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
