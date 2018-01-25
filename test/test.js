cc2b60cb5f283c806a00d326e6ce1af01d7d6348
matlittle <matlittle727@gmail.com>
2018-01-24 19:05:55 -0600

    Added testing dev dependencies, and basic test file

---
+++ test/test.js
@@ -0,0 +1,32 @@
+/* Testing Dependencies */
+const Nightmare = require('nightmare');
+const expect    = require('chai').expect;
+
+const nightmare = Nightmare( {show: true} );
+
+
+
+/* describe("Loading google", function() {
+
+    this.timeout('30s')
+
+    let nightmare = null
+    beforeEach(() => {
+      nightmare = new Nightmare()
+    });
+
+    it("Should load the page", done => {
+      nightmare
+        .goto('https://www.google.com')
+        .end()
+        .then(function(result) { done() })
+        .catch(done);
+    });
+  });
+*/
+
+
+
+
+
+
