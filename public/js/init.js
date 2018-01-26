639c9cf7577f199cf52e024b206170e5d513fe82
Alex Parris <alex@econsortium.com>
2018-01-25 20:41:34 -0600

    modals work!

--- public/js/init.js
+++ public/js/init.js
@@ -1,7 +1,27 @@
 (function($){
-  $(function(){
+  $.fn.leanModal = function(options) {
+    if( $('.modal').length > 0 ){
+        $('.modal').modal(options);
+    }
+  };
 
-    $('.button-collapse').sideNav();
+  $.fn.openModal = function(options) {
+    $(this).modal(options);
+    $(this).modal('open');
+  };
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  $.fn.closeModal = function() {
+    $(this).modal('close');
+  };
+
+  $('.modal-trigger').leanModal({
+    dismissible: false, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration
+    backdrop: 'static', 
+    keyboard: false
+  }
+);
+    
+})(jQuery);
